390eb6e763eab5a7f2671cf37c471211e47f55d5
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-03-31 10:13:27 +0600

    show the json data on browser

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookJournalWhills } from '@fortawesome/free-solid-svg-icons'
 
 const Blogs = (props) => {
     const {id, authorImage, authorName, blogImage, blogTitle, publishDate, readTime} = props.blog
     console.log(id)
     return (
-        <div className='bg-orange-200'>
+        <div className=''>
             <img className='w-full' src={blogImage} alt="Blog image" />
-            <div className='flex my-3'>
-                <img className='w-12 rounded-full mx-3' src={authorImage} alt="author image" />
+            <div className='flex my-3 justify-between items-center'>
+                <div className='flex'>
+                <img className='w-12 rounded-full mr-5' src={authorImage} alt="author image" />
                 <div>
                     <p className='font-bold text-xl'>{authorName}</p>
-                    <p>{publishDate}</p>
+                    <p className='text-sm text-gray-600'>{publishDate}</p>
                 </div>
-                <p>{readTime} min read</p>
+                </div>
+                <p>{readTime} min read <FontAwesomeIcon icon="fa-regular fa-bookmark" /></p>
             </div>
-            <h3 className='font-bold text-xl'>{blogTitle}</h3>
+            <h3 className='font-bold text-3xl my-4'>{blogTitle}</h3>
             <p>#beginners #programming</p>
-            <p><a className='text-blue-800 underline' href="#">Mark as read</a></p>
+            <p className='my-5'><a className='text-blue-800 underline' href="#">Mark as read</a></p>
+            <hr className='mb-7'/>
         </div>
     );
 };
